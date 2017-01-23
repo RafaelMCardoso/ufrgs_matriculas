f5a1e344413c6db060c306aab3101a5ef463c98c
Rafael Cardoso <rafaeldemoraescardoso@gmail.com>
2017-01-23 21:45:24 -0200

    Função Possibilidade de matriculas - First Commit

---
+++ PosMatFunction.R
@@ -0,0 +1,24 @@
+PosMat<-function(Horav="all",Dia1v=NULL,Dia2v=NULL){
+  data<-read.csv("PossibMatric.CSV")
+ 
+  ##cria a variavel d_data que sera aninhada a variavael h_data para mostrar o resultado final   
+  ##d_data depende dos valores de Dia1v e Dia2V
+  ##Dia2v existe somente quando a cadeira é dividida em 2 dias; portando nunca existira Dia2v se Dia1v for NULL
+  if(is.null(Dia2v)){
+    d_data<-data[data$Dia1==Dia1v,]
+  }
+  else{
+    d_data<-data[data$Dia1==Dia1v&data$Dia2==Dia2v,]
+  }
+  
+  ##se o horario escolhido for "all", o resultado final será todos as cadeira disponíveis naquele semestre
+  if(!Horav=="all"){
+    h_data<-d_data[d_data$Horario==Horav,]
+  }
+    else{
+      h_data<-d_data
+      message("Mostrando todas as aulas para os dias selecionados")
+    }
+  print(h_data)
+  ##End of fuction
+}
