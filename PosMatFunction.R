d09f94b4f253765120f08b105b4cc1cf316f3bfb
Rafael Cardoso <rafaeldemoraescardoso@gmail.com>
2017-01-23 22:10:19 -0200

    ColsAdded

--- PosMatFunction.R
+++ PosMatFunction.R
@@ -1,5 +1,6 @@
 PosMat<-function(Horav="all",Dia1v=NULL,Dia2v=NULL){
   data<-read.csv("PossibMatric.CSV")
+  cols<-c("Horario","Dia1","Dia2","Nome.Extenso","Dep","Creditos")
   ##cria a variavel d_data que sera aninhada a variavael h_data para mostrar o resultado final   
   ##d_data depende dos valores de Dia1v e Dia2V
   ##Dia2v existe somente quando a cadeira é dividida em 2 dias; portando nunca existira Dia2v se Dia1v for NULL
@@ -7,15 +8,15 @@ PosMat<-function(Horav="all",Dia1v=NULL,Dia2v=NULL){
     d_data<-data[data$Dia1==Dia1v,]
   }
   else{
-    d_data<-data[data$Dia1==Dia1v&data$Dia2==Dia2v,]
+    d_data<-data[data$Dia1==Dia1v&data$Dia2==Dia2v,cols]
   }
   
   ##se o horario escolhido for "all", o resultado final será todos as cadeira disponíveis naquele semestre
   if(!Horav=="all"){
-    h_data<-d_data[d_data$Horario==Horav,]
+    h_data<-d_data[d_data$Horario==Horav,cols]
   }
     else{
-      h_data<-d_data
+      h_data<-d_data[,cols]
       message("Mostrando todas as aulas para os dias selecionados")
     }
   print(h_data)
